0080d359f29c691c6d44473f228a7a17cd88495c
RajeshChinta <rajeshreddychinta@gmail.com>
2020-04-26 21:42:17 +0530

    Adding missing class in command pattern code

---
+++ CommandPatternUsingLambdas/src/main/java/com/rajeshchinta/commandpattern/devices/OutDoorLight.java
@@ -0,0 +1,12 @@
+package com.rajeshchinta.commandpattern.devices;
+
+public class OutDoorLight {
+	public void on() {
+		System.out.println("OutDoorLight turned On.");
+	}
+	
+	public void off() {
+		System.out.println("OutDoor turned Off.");
+	}
+
+}
